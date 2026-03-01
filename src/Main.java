de0301176d15c87b5d850c23d9c20ec0504c6417
Ivan <150073370+wasdiezz@users.noreply.github.com>
2026-03-01 12:37:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        String path ="C:\\Users\\vkave\\IdeaProjects\\eneric\\src\\sources\\products.csv";
+        CsvProductReader csvProductReader = new CsvProductReaderImpl();
+        ReadResult<ProductLine> readResult = csvProductReader.read(path);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BillCalculator billCalculator = new BIllCalculatorImpl();
+
+        BillResult<BillLine> billResult = billCalculator.calculate(readResult.getItems());
+
+        CsvBillWriter csvBillWriter = new CsvBillWriterImpl();
+        String outputPath = "C:\\Users\\vkave\\IdeaProjects\\eneric\\src\\bill\\Bill";
+
+        csvBillWriter.write(outputPath, billResult.getLines(),  billResult.getTotal(), readResult.getErrorCount());
+
     }
 }
