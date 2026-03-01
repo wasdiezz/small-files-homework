f5109273ccee69e8f63f4b99f6ef0b91ddc09df6
Ivan <150073370+wasdiezz@users.noreply.github.com>
2026-03-01 12:39:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
-        String path ="C:\\Users\\vkave\\IdeaProjects\\eneric\\src\\sources\\products.csv";
+        System.out.println("а");
+        String path ="C:\\Users\\vkave\\IdeaProjects\\small-files-homework\\src\\sources\\products.csv";
         CsvProductReader csvProductReader = new CsvProductReaderImpl();
         ReadResult<ProductLine> readResult = csvProductReader.read(path);
 
@@ -11,7 +12,7 @@ public class Main {
         BillResult<BillLine> billResult = billCalculator.calculate(readResult.getItems());
 
         CsvBillWriter csvBillWriter = new CsvBillWriterImpl();
-        String outputPath = "C:\\Users\\vkave\\IdeaProjects\\eneric\\src\\bill\\Bill";
+        String outputPath = "C:\\Users\\vkave\\IdeaProjects\\small-files-homework\\src\\end\\Bill.csv";
 
         csvBillWriter.write(outputPath, billResult.getLines(),  billResult.getTotal(), readResult.getErrorCount());
 
